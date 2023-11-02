08bf9ac1d70b2aa0d509f7626431f6b66dd0902b
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-11-03 00:53:59 +0600

    first commit

---
+++ type.js
@@ -0,0 +1,19 @@
+// JavaScript is a dynamic typed Language
+
+// Primitive Type
+const a = 21;
+const b = 'Mufassir';
+const c = true;
+
+
+// Non Primitive
+const  d = [11,22,33,44,55];
+const e ={
+    id: 21,
+    name: 'mufassir'
+}
+
+const x = {job: 'web developer'};
+const y = x;
+y.job = 'Front End Developer'
+console.log(x,y);
