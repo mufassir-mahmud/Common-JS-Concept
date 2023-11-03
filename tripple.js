36a9a7d75887f3e123f3df3455e47df582dc9ab9
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-11-03 17:15:24 +0600

    tripple equal

---
+++ tripple.js
@@ -0,0 +1,29 @@
+const first = 2;
+const second = '2';
+// if(first === second){
+//     console.log('These Are Equal');
+// }
+// else{
+//     console.log('These Are Not Equal');
+// }
+
+const third = 1;
+const fourth = true;
+// if(third == fourth){
+//     console.log('These Are Equal');
+// }
+// else{
+//     console.log('These Are Not Equal');
+// }
+
+// Type coercion
+// always use ===
+
+const fifth = [11,22];
+const sixth = fifth;
+if(fifth === sixth){
+    console.log('These Are Equal');
+}
+else{
+    console.log('These Are Not Equal');
+}
