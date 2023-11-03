cc821d3069dce0d398cfe141a40515cc649ce85a
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-11-03 19:23:12 +0600

    file added

---
+++ hoisting.js
@@ -0,0 +1,8 @@
+for(var i = 0; i< 5; i++){
+    console.log(i);
+}
+console.log(i);
+for(let i = 0; i< 5; i++){
+    console.log(i);
+}
+console.log(i);
---
+++ scope.js
@@ -0,0 +1,12 @@
+function add(a,b){
+    const total = a+b;
+    console.log(a,b);
+    if(b>5){
+        const sum = 30+a+b;
+        console.log(sum);
+    }
+    // console.log(sum);
+    return total
+}
+// console.log(a,b);
+add(5,8)
